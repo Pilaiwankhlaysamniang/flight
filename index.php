1e7948fc9e3cea7185e248053b52f737813b0919
firm1410 <32802031+firm1410@users.noreply.github.com>
2019-10-29 12:57:18 +0700

    test

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 <?php include('../class_conn.php');?>
 <?php $cls_conn=new class_conn;?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!-->
 <html class="no-js">
 <!--<![endif]-->
